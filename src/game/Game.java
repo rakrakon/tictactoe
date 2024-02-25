2e42b94f372c4f92ec05813c6bef0d5a0c8a4ffa
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-02-25 09:27:25 +0200

    Fix gameLogic

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,26 +1,30 @@
 package game;
 
 import game.board.Board;
+import game.board.BoardCoordinates;
 import game.board.Symbol;
+import game.cursor.GameCursor;
 import game.players.PlayerInterface;
 
 import java.io.IOException;
 
 public class Game {
     private final Board board;
-    private final Cursor gameCursor;
+    private final GameCursor gameCursor;
     private PlayerManager playerManager;
+    private TurnManager turnManager;
     private PlayerInterface playerOne;
     private PlayerInterface playerTwo;
 
     public Game(){
         board = Board.getINSTANCE();
-        gameCursor = Cursor.getINSTANCE(3, 3);
+        gameCursor = GameCursor.getINSTANCE(3, 3);
         playerManager = PlayerManager.getINSTANCE();
     }
 
     public void gameInit() throws IOException {
         playerManager.selectPlayers();
+//        turnManager = TurnManager.getInstance(); //TODO: implement logic
     }
 
     public void resetGame(){
@@ -32,16 +36,19 @@ public class Game {
     }
 
     private void printGame(){
-        board.printBoard(gameCursor.getRow(), gameCursor.getCol()); //TODO: call turn manager printTurn function
+        System.out.println(board.getBoard(gameCursor.getRow(), gameCursor.getCol()));
     }
 
     public void start() throws IOException {
         resetGame();
         System.out.println();
-        while (!isGameOver()) { //TODO: refactor the printGame and gameCursor to gameCursor.handleInput and
-            printGame(); //TODO: make enter place the symbol
+        while (!isGameOver()) {
+            printGame();
 
-            gameCursor.handleInput(); //Read keyboard input
+            var cursorValue = gameCursor.handleInput(board.getBoard(gameCursor.getRow(), gameCursor.getCol())); //Read keyboard input
+            if(cursorValue.isPresent()){ //TODO: maybe this needs to be in the player interface? and here the turnManager.playTurn will be called
+                board.setBoardSymbol(new BoardCoordinates(gameCursor.getRow(), gameCursor.getCol()), turnManager.getCurrentPlayerSymbol()); //Place correct symbol on the board
+            }
         }
         System.out.println("Congratulations " + board.getWinner().getSymbolChar() + " won the game!!");
     }
