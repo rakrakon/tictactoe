cd9d52a75ecf581669a2b0800de6ea74780dc69f
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-03-23 14:22:07 +0200

    Add game.newGame to Main

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,11 @@
+
 import game.Game;
-import game.PlayerSelectionMenu;
 
 import java.io.IOException;
 
 public class Main {
-    private static Game game;
-    public static void main(String[] args) throws IOException { //TODO: create game manager class
-        PlayerSelectionMenu playerSelectionMenu = PlayerSelectionMenu.getINSTANCE();
-        playerSelectionMenu.selectPlayers();
+    public static void main(String[] args) throws IOException {
+        Game game = new Game();
+        game.newGame();
     }
 }
