6bc797d23481c13c449b1d01ed1bd7bb03ea5874
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-03-23 14:27:29 +0200

    Add Player.playTurn logic

--- src/game/players/Player.java
+++ src/game/players/Player.java
@@ -1,7 +1,11 @@
 package game.players;
 
-import game.GameInputs;
+import game.board.Board;
+import game.board.BoardCoordinates;
 import game.board.Symbol;
+import game.cursor.Cursor;
+
+import java.io.IOException;
 
 public class Player implements PlayerInterface{
     private final Symbol playerType;
@@ -14,9 +18,28 @@ public class Player implements PlayerInterface{
     }
 
     @Override
-    public void playTurn() {
-        char symbol = playerType.getSymbolChar();
-        System.out.println("Please enter the coordinates (row and column) to place your mark.");
-        System.out.println("Coordinates should be in the range 0-2, 0-2, starting from the top left corner.");
+    public void playTurn() throws IOException {
+        Cursor gameCursor = Cursor.getINSTANCE2();
+        Board board = Board.getINSTANCE();
+        while (true) {
+            System.out.println(board.getBoard(gameCursor.getRow(), gameCursor.getCol()));
+            var cursorValue = gameCursor.handleInput(board.getBoard(gameCursor.getRow(), gameCursor.getCol())); //Read keyboard input
+            if (cursorValue.isPresent()) {
+                if (cursorValue.get().equals(Symbol.EMPTY.getSymbolChar())) {
+                    board.setBoardSymbol(new BoardCoordinates(gameCursor.getRow(), gameCursor.getCol()), playerType); //Place correct symbol on the board
+                    return;
+                }
+                System.out.println("There is already a symbol there try again:");
+            }
+        }
+    }
+
+    public static String getName() {
+        return "Human Player";
+    }
+
+    @Override
+    public Symbol getPlayerSymbol() {
+        return playerType;
     }
 }
